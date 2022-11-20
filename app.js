af03e29da27f5514970ae985fdd40610a3519a2b
John Kerama johnnesta2018@gmail.com <johnnesta2018@gmail.com>
2022-11-20 10:21:29 +0300

    Finishing up on the base API architecture

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { CancellationRoute, CustomerRoute, JobRoute, ReviewRoute } = require('./routers');
+
+
+const PORT = +process.env.PORT || 3300;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/cancellations', CancellationRoute);
+app.use('/customer', CustomerRoute);
+app.use('/jobs', JobRoute);
+app.use('/reviews', ReviewRoute);
+
+
+;(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        app.listen(PORT, () => {
+            console.log(`Server started on http://localhost:${PORT}`);
+        })
+
+    } catch(error) {
+        console.log("Failed to connect to the DB");
+        console.error(error);
+    }
+})()
--- routers/index.js
+++ routers/index.js
@@ -1,1 +1,8 @@
 // entry point 
+module.exports = {
+    CancellationRoute: require('./cancellations'),
+    CustomerRoute: require('./customers'),
+    JobRoute: require('./jobs'),
+    MerchantRoute: require('./merchants'),
+    ReviewRoute: require('./reviews')
+}
